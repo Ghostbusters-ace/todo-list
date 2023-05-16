ffb49166fc1fb58e01a60aa725371be1447577c5
lionj <judin.malivert@poe.aelion.biz>
2023-05-16 11:16:32 +0200

    electron build in package

--- electron/main.ts
+++ electron/main.ts
@@ -16,7 +16,7 @@ function createWindow() {
 
   mainWindow.loadURL(
     url.format({
-      pathname: path.join(__dirname, 'index.html'),
+      pathname: path.join(__dirname, 'todo/index.html'),
       protocol: 'file:',
       slashes: true,
     })
